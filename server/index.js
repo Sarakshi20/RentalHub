ba9eac326b18749d935a57d7bf19a17c26de1e87
Sarakshi20 <sarakshi1275.be21@chitkara.edu.in>
2024-02-14 09:33:02 +0530

    More Deployment Changes

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow your frontend origin
+    origin: 'https://rental-hub-topaz.vercel.app/', // Allow your frontend origin
     methods: ['GET', 'POST'], // Allowable methods
 }));
 
